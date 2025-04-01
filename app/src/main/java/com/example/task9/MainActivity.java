4a63dbc494a3a7c105a0d2a49216426e9ff77164
sp6822 <sp6822@bs.amalne.k12.il>
2025-04-02 00:52:35 +0300

    Initial commit

---
+++ app/src/main/java/com/example/task9/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.task9;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText address;
+    WebView wb;
+    Button btnGo;
+    String stringUrl;
+
+    
